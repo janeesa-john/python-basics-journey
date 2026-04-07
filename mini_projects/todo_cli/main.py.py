ddacfe12a004d76177b12490a35211cb04154f82
janeesa-john <janeesajon@gmail.com>
2026-04-07 22:26:13 +0530

    Day 6: Set up CLI structure for To-Do App with menu options

---
+++ mini_projects/todo_cli/main.py.py
@@ -0,0 +1,25 @@
+#CLI To-Do App
+
+while True:
+    print("\nTo-Do App")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+    # add task
+
+    elif choice == "2":
+    # view tasks
+
+    elif choice == "3":
+    # delete task
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice")
