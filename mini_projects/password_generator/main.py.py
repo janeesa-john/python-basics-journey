f906533b79b7bc5297fd04497f32c3aab052fea4
janeesa-john <janeesajon@gmail.com>
2026-04-11 13:17:27 +0530

    Add: Implemented password generator with repeat option

--- mini_projects/password_generator/main.py.py
+++ mini_projects/password_generator/main.py.py
@@ -1,3 +1,28 @@
-# Password Generator (Coming Soon)
+# Password Generator
 
-print("Password Generator - Work in progress")
+
+import random
+import string
+
+print("🔐 Password Generator")
+
+while True:
+    length = int(input("Enter password length: "))
+
+    if length <= 0:
+        print("Enter a valid length")
+        continue
+
+    chars = string.ascii_letters + string.punctuation + string.digits
+    password = ''
+
+    for i in range(length):
+        password+= random.choice(chars)
+
+    print(f"Generated password: {password}")
+
+    again = input("Generate again?(yes/no): ").lower()
+
+    if again != 'yes':
+        print("Good Bye!")
+        break
